eaabd4c3029de2ae01c90a239ed1a5ea8c88ff30
NelsonFrancis <developer.nelson22@gmail.com>
2025-06-27 19:47:43 +0400

    email auth done

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,5 +1,20 @@
-export default function Login(){
+"use client"
+
+import AuthForm from "../AuthForm"
+
+export default function Login() {
+
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault()
+
+    console.log(email, password)
+  }
+
   return (
-    <div>Login</div>
+    <main>
+      <h2 className="text-center">Login</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
+    </main>
   )
 }
