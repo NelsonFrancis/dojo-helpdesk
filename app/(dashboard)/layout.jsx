ef550bd388702cb96996a28cf9a8527bb32fc9c7
NelsonFrancis <developer.nelson22@gmail.com>
2025-07-01 17:04:17 +0400

    protecting routes

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,11 +1,15 @@
 import Navbar from "../components/Navbar";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({children}){
     const supabase = createServerComponentClient({cookies});
-    const {data} = await supabase.auth.getSession()
-    console.log("dat==",data)
+    const {data} = await supabase.auth.getSession();
+    console.log("dat==",data);
+    if(!data.session){
+        redirect("/login")
+    }
     return(
         <>
             <Navbar  userData={data.session.user}/>
