3b94043818c45e8d320d304550cabb2c8cac78d6
NelsonFrancis <developer.nelson22@gmail.com>
2025-07-07 12:53:14 +0400

    code update

--- app/(dashboard)/tickets/create/CreateForm.jsx
+++ app/(dashboard)/tickets/create/CreateForm.jsx
@@ -15,18 +15,18 @@ export default function CreateForm() {
     e.preventDefault()
     setIsLoading(true)
 
-    const newTicket = { title, body, priority, user_email: 'mario@netninja.dev' }
+    const newTicket = { title, body, priority}
 
-    const res = await fetch('http://localhost:4000/tickets', {
+    const res = await fetch('http://localhost:3000/api/tickets', {
       method: "POST",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify(newTicket)
     })
 
-    if (res.status === 201) {
-      router.refresh()
-      router.push('/tickets')
-    }
+    // if (res.status === 201) {
+    //   router.refresh()
+    //   router.push('/tickets')
+    // }
     
   }
 
--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -2,28 +2,28 @@ import { NextResponse } from "next/server";
 
 // export const dynamic = "force=dynamic"
 
-export async function GET() {
-    const res = await fetch("http://localhost:4000/tickets");
-    const tickets = await res.json();
+// export async function GET() {
+//     const res = await fetch("http://localhost:4000/tickets");
+//     const tickets = await res.json();
 
-    return NextResponse.json(tickets, {
-        status: 200
-    })
-}
+//     return NextResponse.json(tickets, {
+//         status: 200
+//     })
+// }
 
 
 export async function POST(request){
     const ticket = await request.json();
 
-    const res = await fetch("http://localhost:4000/tickets", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(ticket)
-    })
+    // const res = await fetch("http://localhost:4000/tickets", {
+    //     method: "POST",
+    //     headers: {"Content-Type": "application/json"},
+    //     body: JSON.stringify(ticket)
+    // })
 
-    const newTicket = await res.json();
+    // const newTicket = await res.json();
 
-    return NextResponse.json(newTicket, {
-        status: 201
-    })
+    // return NextResponse.json(newTicket, {
+    //     status: 201
+    // })
 }
